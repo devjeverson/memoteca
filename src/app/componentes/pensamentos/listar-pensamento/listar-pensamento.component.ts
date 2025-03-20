0fb3c2e55f5543641130f72afeeb27f2e3cac5c9
devjeverson <developer.oliveirajv@gmail.com>
2025-03-20 18:35:50 -0300

    add json server

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -18,6 +18,12 @@ export class ListarPensamentoComponent implements OnInit {
     //autoria: 'Componete filho',
     // modelo: 'modelo2'
     //}
+    // {
+    //   conteudo: 'lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi.lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi',
+    //   autoria: 'Componete Pai',
+    //   modelo: 'modelo1'
+    // },
+   
   ];
 
   constructor() { }
